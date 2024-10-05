907dfe380bef6b5ab28d95b16c673e25955eba50
Connor Moodie <26518925+cmoodie@users.noreply.github.com>
2024-10-05 15:57:53 +1000

    Changed outdir back to dist

--- vite-ts-app/vite.config.ts
+++ vite-ts-app/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '/var/www/decorum.connormoodie.com/html',
+    outDir: 'dist',
     emptyOutDir: true
   }
 })
